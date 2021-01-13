bb5d7824a01f68740e40b3f456a8b6a8a4f3c12c
Arva Kagdi <arvamkagdi@gmail.com>
2021-01-12 21:30:17 -0500

    Created Styling for Keeper App

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header.jsx';
+import Footer from './footer.jsx';
+import Note from './note.jsx';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+            <Note />
+        </div>
+    );
+}
+
+export default App;
